9759f6b585869aad77e81bb0a0a9cd6482bc3f30
andrea-scm <a.scimitto@gmail.com>
2019-04-15 21:03:02 +0200

    done

---
+++ pswgen_script.js
@@ -0,0 +1,11 @@
+var name,surname,fav_color,rnd_num;
+name = prompt('Inserisci il tuo nome');
+surname = prompt('Inserisci il tuo cognome');
+fav_color = prompt('Inserisci il tuo colore preferito');
+console.log(name);
+console.log(surname);
+console.log(fav_color);
+rnd_num = Math.floor(((Math.random() * 98) + 1)); // *98 + 1 cosi prende i numeri random da 1 a 99
+console.log(rnd_num);
+//document.getElementById('rnd_psw').innertHTML = name + surname + fav_color + rnd_num; ??non funziona
+document.writeln(name + surname + fav_color + rnd_num);
